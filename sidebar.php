d3b5472ca02d84619f49b58f98072ec649b23773
randyjensen <randy@randyjensenonline.com>
2011-02-01 23:18:58 -0600

    fixed h1 mix up in sidebar

--- sidebar.php
+++ sidebar.php
@@ -12,14 +12,14 @@
 				</aside>
 
 				<aside id="archives" class="widget" role="complementary">
-					<h1 class="widget-title"><?php _e( 'Archives', 'themename' ); ?></h2>
+					<h2 class="widget-title"><?php _e( 'Archives', 'themename' ); ?></h2>
 					<ul>
 						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 					</ul>
 				</aside>
 
 				<aside id="meta" class="widget" role="complementary">
-					<h1 class="widget-title"><?php _e( 'Meta', 'themename' ); ?></h2>
+					<h2 class="widget-title"><?php _e( 'Meta', 'themename' ); ?></h2>
 					<ul>
 						<?php wp_register(); ?>
 						<aside role="complementary"><?php wp_loginout(); ?></aside>
