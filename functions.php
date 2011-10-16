b6d80be5d75f33659fa99970d4d1de211fad1f68
Randy Jensen <randy@randyjensenonline.com>
2011-10-16 18:21:31 -0500

    updated TEMPLATEPATH to get_template_directory()

--- functions.php
+++ functions.php
@@ -8,10 +8,10 @@
  * Make theme available for translation
  * Translations can be filed in the /languages/ directory
  */
-load_theme_textdomain( 'themename', TEMPLATEPATH . '/languages' );
+load_theme_textdomain( 'themename', get_template_directory() . '/languages' );
 
 $locale = get_locale();
-$locale_file = TEMPLATEPATH . "/languages/$locale.php";
+$locale_file = get_template_directory() . "/languages/$locale.php";
 if ( is_readable( $locale_file ) )
 	require_once( $locale_file );
 
